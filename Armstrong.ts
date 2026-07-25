b345621ad0faf5a20bf561acc275d013bdf5c436
skathyrene-386 <suzanne.thodeti@gmail.com>
2026-07-25 08:15:40 +0530

    Armstrong.ts

---
+++ Armstrong.ts
@@ -0,0 +1,15 @@
+let number: number = 153;
+let temp = number;
+let sum = 0;
+
+while (temp > 0) {
+    let digit = temp % 10;
+    sum += digit * digit * digit;
+    temp = Math.floor(temp / 10);
+}
+
+if (sum === number) {
+    console.log(number + " is an Armstrong Number");
+} else {
+    console.log(number + " is not an Armstrong Number");
+}

d648bd6d03a8e28fd6dc422168f9a7ba0da6b5d2
skathyrene-386 <suzanne.thodeti@gmail.com>
2026-08-08 16:43:30 +0530

    Add files via upload

---
+++ week1/Exp1.js
@@ -0,0 +1,50 @@
+"use strict";
+// string
+let message = "Hello, TypeScript World qwertyuiop[asdfghjklzxcvbnm, qwertyu dfghj cvbnm jkt!";
+console.log(message);
+let city = "Hyderabad";
+console.log(city);
+let msg = "Hello Vishnu Engineering College";
+console.log(msg);
+// number
+let age = 20;
+console.log(age);
+let pi = 3.14;
+console.log(pi);
+let mobile = 7995951727;
+console.log(mobile);
+// boolean
+let isPass = true;
+console.log(isPass);
+let isOnline = false;
+console.log(isOnline);
+let hasId = true;
+console.log(hasId);
+// null
+let data = null;
+console.log(data);
+let rollno = null;
+console.log(rollno);
+let address = null;
+console.log(address);
+// undefined
+let x = undefined;
+console.log(x);
+let y = undefined;
+console.log(y);
+let z = undefined;
+console.log(z);
+// bigint
+let big = 50n;
+console.log(big);
+let num = 100000000000n;
+console.log(num);
+let value = 99n;
+console.log(value);
+// Symbol
+let id = Symbol("id");
+console.log(id);
+let key = Symbol("key");
+console.log(key);
+let token = Symbol("token");
+console.log(token);
